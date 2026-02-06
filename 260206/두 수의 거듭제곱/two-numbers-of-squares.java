728506835a3f6ba55f815d78042a4e45aa5c7ed4
sanghyeon shin <tlstkdgus9@naver.com>
2026-02-06 16:02:28 +0900

    Title: 두 수의 거듭제곱, Time: 133ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260206/두 수의 거듭제곱/two-numbers-of-squares.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(power(a, b));
+    }
+
+    public static int power(int a, int b){
+        if(b == 0){
+            return 1;
+        }
+        else{
+            return a * power(a, b-1);
+        }
+
+    }
+}
