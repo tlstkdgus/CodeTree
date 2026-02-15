216b64abc6791b3947492d15ec421db073e82e3f
sanghyeon shin <tlstkdgus9@naver.com>
2026-02-15 12:58:38 +0900

    Title: 대문자로 바꾸기, Time: 118ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260215/대문자로 바꾸기/change-to-capital.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        for(int i=0; i<5; i++){
+            System.out.println(sc.nextLine().toUpperCase());
+        }
+
+
+
+    }
+}
