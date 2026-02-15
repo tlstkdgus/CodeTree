5cba49a3873693e386d13082141782b4b9fe0807
sanghyeon shin <tlstkdgus9@naver.com>
2026-02-15 18:00:41 +0900

    Title: 배열 채우고 출력하기, Time: 125ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260215/배열 채우고 출력하기/filling-array-and-print.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] str = new String[10];
+        for(int i=0; i<10; i++){
+            str[i] = sc.next();
+        }
+
+        for(int i=9; i>=0; i--){
+            System.out.print(str[i]);
+        }
+    }
+}
