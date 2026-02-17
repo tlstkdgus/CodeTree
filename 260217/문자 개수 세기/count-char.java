0d5c4ef16d23d5a32e1c4d9d22009a06d2e8ea70
sanghyeon shin <tlstkdgus9@naver.com>
2026-02-17 17:03:55 +0900

    Title: 문자 개수 세기, Time: 123ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260217/문자 개수 세기/count-char.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+        String alp = sc.next();
+        int count = 0;
+        for(int i=0; i<str.length(); i++){
+            if(str.charAt(i) == alp.charAt(0)){
+                count++;
+            }
+        }
+        System.out.println(count);
+        
+    }
+}
