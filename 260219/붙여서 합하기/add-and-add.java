c14912f21ce2184e0697e67595254c8260ffe56f
sanghyeon shin <tlstkdgus9@naver.com>
2026-02-19 01:37:23 +0900

    Title: 붙여서 합하기, Time: 157ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260219/붙여서 합하기/add-and-add.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str1 = sc.next();
+        String str2 = sc.next();
+        System.out.print(Integer.parseInt(str1+str2)+Integer.parseInt(str2+str1));
+
+    }
+}
