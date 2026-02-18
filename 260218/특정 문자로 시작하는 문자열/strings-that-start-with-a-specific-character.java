26ecb1d354f978d4fb6a8fdae5045cfabe7f078b
sanghyeon shin <tlstkdgus9@naver.com>
2026-02-18 14:41:40 +0900

    Title: 특정 문자로 시작하는 문자열, Time: 132ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260218/특정 문자로 시작하는 문자열/strings-that-start-with-a-specific-character.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc =  new Scanner(System.in);
+
+        int n = sc.nextInt();
+        String[] arr = new String[n];
+        for(int i=0; i<n; i++){
+            arr[i] = sc.next();
+        }
+        String ans = sc.next();
+        int count = 0;
+        double length = 0;
+        for(int i=0; i<n; i++){
+            if(arr[i].charAt(0) == ans.charAt(0)){
+                count++;
+                length+= arr[i].length();
+            }
+        }
+        System.out.print(count + " ");
+        System.out.printf("%.2f", length/count);
+    }
+}
