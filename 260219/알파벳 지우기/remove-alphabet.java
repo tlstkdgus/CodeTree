66b97f6a872d738b250da8b66269162c245290b8
sanghyeon shin <tlstkdgus9@naver.com>
2026-02-19 01:49:54 +0900

    Title: 알파벳 지우기, Time: 118ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260219/알파벳 지우기/remove-alphabet.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str1 = sc.next();
+        String str2 = sc.next();
+        int ans1 = extractNum(str1);
+        int ans2 = extractNum(str2);
+        System.out.println(ans1 + ans2);
+
+
+    }
+
+    public static int extractNum(String str){
+        String ans = "";
+        for(int i=0; i<str.length(); i++){
+            char ch = str.charAt(i);
+            if(ch>='0' && ch<='9'){
+                ans+= ch;
+            }
+        }
+        return (Integer.parseInt(ans));
+    }
+}
