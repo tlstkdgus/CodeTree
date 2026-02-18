eaf1d7eb9bc28cdbe72293575953f5a22ede3579
sanghyeon shin <tlstkdgus9@naver.com>
2026-02-19 00:58:58 +0900

    Title: 아스키코드 변환, Time: 125ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260219/아스키코드 변환/convert-to-ascii.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print((int)sc.next().charAt(0) + " ");
+        System.out.print((char)sc.nextInt());
+    }
+}
