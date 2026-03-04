3e6d6cda6da7edf3fcd714975519046db004e0fb
sanghyeon shin <tlstkdgus9@naver.com>
2026-03-04 13:59:45 +0900

    Title: 동전 더하기, Time: 125ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260304/동전 더하기/add-coins.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] coins = new int[n];
+        for (int i = 0; i < n; i++) {
+            coins[i] = sc.nextInt();
+        }
+
+        int count = 0;
+
+        for(int i=n-1; i>0; i--){
+            int use = k/coins[i];
+            count += use;
+            k %= coins[i];
+        }
+
+        System.out.println(count);
+    }
+}
