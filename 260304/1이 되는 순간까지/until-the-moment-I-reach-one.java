b7fbc558e65001e3270735151041fbb9b5651261
sanghyeon shin <tlstkdgus9@naver.com>
2026-03-04 12:00:57 +0900

    Title: 1이 되는 순간까지, Time: 126ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260304/1이 되는 순간까지/until-the-moment-I-reach-one.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(divide(n));
+    }
+
+    public static int divide(int n){
+        if(n==1){
+            return 0;
+        }
+        if(n%2==0){
+            return 1+ divide(n/2);
+        }else{
+            return 1+ divide(n/3);
+        }
+    }
+}
