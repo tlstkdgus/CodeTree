4f835ddd9ce8fcf2924df357536b3adcfcd8bd40
sanghyeon shin <tlstkdgus9@naver.com>
2026-03-05 10:07:43 +0900

    Title: 삽입 정렬 구현, Time: 133ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260305/삽입 정렬 구현/implement-insertion-sort.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for(int i=0; i<arr.length; i++){
+            int j= i-1;
+            int key = arr[i];
+            while(j>=0 && arr[j] > key){
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = key;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < arr.length; i++) {
+            sb.append(arr[i]);
+            if (i < arr.length - 1) sb.append(" ");
+        }
+        System.out.println(sb.toString());
+    }
+}
