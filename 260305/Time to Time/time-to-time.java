8411fc1b6b718b65a8f48563b69277c00358fe5f
sanghyeon shin <tlstkdgus9@naver.com>
2026-03-05 16:10:17 +0900

    Title: Time to Time, Time: 128ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260305/Time to Time/time-to-time.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+        
+        System.out.println((c*60 +d) - (a*60 +b));
+    }
+}
