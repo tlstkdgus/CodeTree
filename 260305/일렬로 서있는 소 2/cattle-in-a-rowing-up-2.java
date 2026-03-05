ac30147a2cbc2246573328e528a5d05d4ebecc0f
sanghyeon shin <tlstkdgus9@naver.com>
2026-03-05 17:01:05 +0900

    Title: 일렬로 서있는 소 2, Time: 127ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260305/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+        
+        int count = 0;
+
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                for(int k=j+1; k<n; k++){
+                    if(arr[i]<= arr[j] && arr[j]<= arr[k]){
+                        count++;
+                    }
+                }
+            }
+        }
+
+        System.out.println(count);
+
+    }
+}
