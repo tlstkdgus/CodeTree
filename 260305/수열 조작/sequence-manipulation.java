fbff0dbed14901786f1c7646732500d464359aa7
sanghyeon shin <tlstkdgus9@naver.com>
2026-03-05 16:05:23 +0900

    Title: 수열 조작, Time: 164ms, Memory: 13MB, Status: Passed - Codetree

---
+++ 260305/수열 조작/sequence-manipulation.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.util.Deque;
+import java.util.ArrayDeque;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        Deque<Integer> dq = new ArrayDeque<>();
+
+        for(int i=1; i<=n; i++){
+            dq.addLast(i);
+        }
+
+        while(dq.size()!=1){
+            dq.pollFirst();
+            dq.addLast(dq.pollFirst());
+        }
+        System.out.println(dq.pollFirst());
+        
+    }
+}
