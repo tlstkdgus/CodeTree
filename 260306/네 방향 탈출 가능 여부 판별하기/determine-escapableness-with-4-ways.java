faa8590156e52e0e672bebf5de4981b31af6fdf6
sanghyeon shin <tlstkdgus9@naver.com>
2026-03-06 17:47:44 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: 305ms, Memory: 21MB, Status: Passed - Codetree

--- 260306/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.java
+++ 260306/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.java
@@ -1,53 +1,60 @@
-import java.util.Scanner;
+import java.util.*;
+
 public class Main {
+    static int n, m;
+    static int[][] grid;
+    static boolean[][] visited;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        int[][] grid = new int[n][m];
+        n = sc.nextInt();
+        m = sc.nextInt();
+        grid = new int[n][m];
+        visited = new boolean[n][m];
+
         for (int i = 0; i < n; i++)
             for (int j = 0; j < m; j++)
                 grid[i][j] = sc.nextInt();
-        boolean[][] visited = new boolean[n][m];
 
-        if(grid[0][0] == 0){
+        if (grid[0][0] == 0) {
             System.out.println(0);
             return;
         }
+
         BFS();
     }
 
     public static void BFS() {
-        //상, 하 , 좌, 우
-        int[] dr = new int[]{-1,1,0,0};
-        int[] dc = new int[]{0,0,-1,1};
+        int[] dr = {-1, 1, 0, 0};
+        int[] dc = {0, 0, -1, 1};
 
         Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{0,0});
+        queue.add(new int[]{0, 0});
         visited[0][0] = true;
 
-        while(!queue.isEmpty()){
+        while (!queue.isEmpty()) {
             int[] cur = queue.poll();
             int r = cur[0];
             int c = cur[1];
 
-            if(r == N-1 && c== M-1){
+            if (r == n-1 && c == m-1) {
                 System.out.println(1);
                 return;
             }
 
-            for(int d=0; d<4; d++){
+            for (int d = 0; d < 4; d++) {
                 int nr = r + dr[d];
                 int nc = c + dc[d];
+
+                if (nr < 0 || nr >= n || nc < 0 || nc >= m) continue;
+                if (grid[nr][nc] == 0) continue;
+                if (visited[nr][nc]) continue;
+
+                visited[nr][nc] = true;
+                queue.add(new int[]{nr, nc});
             }
+        }
 
-            if(nr<0 || nr >=n || nc<0 || nc>= M) continue;
-            if(grid[nr][nc] == 0) continue;
-            if(visited[nr][nc]) continue;
-
-            visited[nr][nc] = true;
-            queue.add(new int[]{nr, nc});
-
-
+        System.out.println(0);
     }
 }
